b09c82d898a60b83e81a33e53050b93ccc751702
ADITHYA ACHARYA <49233178+Adithya707@users.noreply.github.com>
2021-12-06 11:22:21 +0530

    Add files via upload

--- source_specified_Dij.c
+++ source_specified_Dij.c
@@ -1,29 +1,37 @@
 #include<stdio.h>
-#include<conio.h>
+
 #define INFINITY 9999
 #define MAX 10
  
-void dijikstra(int G[MAX][MAX], int n, int startnode);
+void dijikstra(int G[MAX][MAX], int n, int startnode,int seq[MAX][MAX]);
  
 void main(){
-	int G[MAX][MAX], i, j, n, u;
-	clrscr();
+	int G[MAX][MAX],seq[MAX][MAX],i, j, n, u;
+	
 	printf("\nEnter the no. of vertices:: ");
 	scanf("%d", &n);
+	for(i=0;i<n;i++){
+           seq[i][i] = 2*i*100;
+        }
 	printf("\nEnter the adjacency matrix::\n");
 	for(i=0;i < n;i++)
-		for(j=0;j < n;j++)
+		for(j=0;j < n;j++){
+			printf("\n Enter Distance between %d to %d :",i,j);
 			scanf("%d", &G[i][j]);
+			if(G[i][j]!=0 && i!=j){
+                           seq[i][j] = seq[i][i];
+                       }
+		}
 	printf("\nEnter the starting node:: ");
 	scanf("%d", &u);
-	dijikstra(G,n,u);
-	getch();
+	dijikstra(G,n,u,seq);
+	
 }
  
-void dijikstra(int G[MAX][MAX], int n, int startnode)
+void dijikstra(int G[MAX][MAX], int n, int startnode,int seq[MAX][MAX])
 {
 	int cost[MAX][MAX], distance[MAX], pred[MAX];
-	int visited[MAX], count, mindistance, nextnode, i,j;
+	int visited[MAX], count, mindistance, nextnode, i,j,k;
 	for(i=0;i < n;i++)
 		for(j=0;j < n;j++)
 			if(G[i][j]==0)
@@ -55,21 +63,36 @@ void dijikstra(int G[MAX][MAX], int n, int startnode)
 				{
 					distance[i]=mindistance+cost[nextnode][i];
 					pred[i]=nextnode;
+					for(k=0;k<n;k++){
+					   seq[i][k] = seq[i][k]+2;
+					}
 				}
 			count++;
 	}
  
 	for(i=0;i < n;i++)
-		if(i!=startnode)
-		{
-			printf("\nDistance of %d = %d", i, distance[i]);
+		if(i != startnode)
+		{      
+			printf("\n Destination %d Distance from %d as starting node = %d", i, startnode, distance[i]);
 			printf("\nPath = %d", i);
 			j=i;
+			k=0;
 			do
 			{
+			        k++;
 				j=pred[j];
-				printf(" <-%d", j);
+				printf("<-%d",j);
 			}
 			while(j!=startnode);
+			printf("\n");
+			printf("Required number of Hops: %d \n",k);
+			printf("\n");
 		}
+        printf("Sequence matrix \n");
+        for(i=0;i<n;i++){
+           for(j=0;j<n;j++){
+              printf("%d\t",seq[i][j]);
+           }
+           printf("\n");
+        }
 }
